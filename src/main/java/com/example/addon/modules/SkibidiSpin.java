28cc5cfca1925971cc881967fe668686c232eccf
punchy <104541798+Nuclearpotato69@users.noreply.github.com>
2024-04-25 17:17:33 -0600

    Update SkibidiSpin.java

--- src/main/java/com/example/addon/modules/SkibidiSpin.java
+++ src/main/java/com/example/addon/modules/SkibidiSpin.java
@@ -27,7 +27,7 @@ public class FloRida extends Module {
     );
 
     private int count = 0;
-    public FloRida() {super(NumbyHack.CATEGORY, "SkibidiSpin", "Makes you spin right round.");}
+    public FloRida() {super(skibidi.client.CATEGORY, "SkibidiSpin", "Makes you spin right round.");}
 
     @EventHandler
     public void onTick(TickEvent.Post event) {
