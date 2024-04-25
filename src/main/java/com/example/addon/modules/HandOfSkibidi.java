4316e97871f22f9a6470fdda05c4aad9cdb3324f
punchy <104541798+Nuclearpotato69@users.noreply.github.com>
2024-04-25 17:17:02 -0600

    Update HandOfSkibidi.java

--- src/main/java/com/example/addon/modules/HandOfSkibidi.java
+++ src/main/java/com/example/addon/modules/HandOfSkibidi.java
@@ -369,7 +369,7 @@ public class HandOfGod extends Module {
             .build()
     );
     public HandOfGod() {
-        super(Trouser.Main, "HandOfSkibidi", "Modify the world and troll players with automated /fill commands. (Must have OP status)");
+        super(skibidi.client.Main, "HandOfSkibidi", "Modify the world and troll players with automated /fill commands. (Must have OP status)");
     }
     private CopyOnWriteArrayList<PlayerListEntry> players;
     private int ticks=0;
